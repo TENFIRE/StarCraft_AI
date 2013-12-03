c58a8d5e4156036ea479ff2537b7d1f28a201843
CarlRapp <Carl.Sebastian.Rapp@gmail.com>
2013-12-03 19:10:19 +0100

    Early build order.

--- BTHAIModule/Source/TerranStrategy.cpp
+++ BTHAIModule/Source/TerranStrategy.cpp
@@ -4,27 +4,58 @@
 #include "ExplorationSquad.h"
 #include "BuildingPlacer.h"
 #include "ExplorationManager.h"
+#include "MarineSquad.h"
 
+
+Squad*	rushSquad;
 TerranStrategy::TerranStrategy()
 {
+	// Supply Depots
 	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 9));
-	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Barracks, 9));
-	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Barracks, 12));
-	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 12));
-	
-	mainSquad = new Squad(1, Squad::OFFENSIVE, "MainSquad", 10);
-	mainSquad->addSetup(UnitTypes::Terran_Marine, 5);
-	mainSquad->setRequired(true);
-	mainSquad->setBuildup(false);
-	squads.push_back(mainSquad);
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 11));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 13));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 20));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 28));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 34));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 40));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 46));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 53));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Supply_Depot, 60));
 
-	noWorkers = 16;
+	//	Barracks
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Barracks, 10));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Barracks, 12));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Barracks, 14));
+
+
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Engineering_Bay, 18));
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Refinery, 20));
+	buildplan.push_back(BuildplanEntry(UpgradeTypes::Terran_Infantry_Weapons, 22));
+	buildplan.push_back(BuildplanEntry(UpgradeTypes::Terran_Infantry_Armor, 24));
+
+	buildplan.push_back(BuildplanEntry(UnitTypes::Terran_Academy, 28));
+	buildplan.push_back(BuildplanEntry(TechTypes::Stim_Packs, 30));
+
+
+	
+	rushSquad = new MarineSquad( 1, Squad::OFFENSIVE, "MarineSquad", 10 );
+	rushSquad->addSetup( UnitTypes::Terran_Marine, 10 );
+	rushSquad->addSetup( UnitTypes::Terran_Medic, 10 );
+	rushSquad->setRequired( true );
+	rushSquad->setBuildup( false );
+	rushSquad->setGoal( findChokePoint() );
+	squads.push_back( rushSquad );
+
+
+
+
+	noWorkers = 18;
 	noWorkersPerRefinery = 2;
 }
 
 TerranStrategy::~TerranStrategy()
 {
-	for (int i = 0; i < (int)squads.size(); i++)
+	for ( int i = 0; i < (int)squads.size(); i++ )
 	{
 		delete squads.at(i);
 	}
